6f13eda87b613bf894d642da3d02870ffeb56b82
bryce-server <bryceness@hotmail.com>
2017-03-16 17:39:53 +0000

    added reset button, see notes in index.js

--- index.js
+++ index.js
@@ -18,21 +18,32 @@ app.get('/chat', function(req, res){
 //socket.on('disconnect') = when disconnect detected
 io.on('connection', function(socket){
   console.log('a user connected');
-  io.emit('allColors', colors);
+  io.emit('allColors', colors);				//tell everyone what the board looks like before they do anything
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
+  //this function is for the chat app
   socket.on('chat message', function(msg) {
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
 
+  //this function is for the colors, it saves the input to "colors" and tells everyone 
   socket.on('colorMessage', function(colorMsg) {
     colors.push({id: colorMsg['id'], color: colorMsg['color']});
     console.log(colors);
     io.emit('allColors', colors);
   });
+  
+  //this resets everything, in case we need to clear the board
+  socket.on('reset', function(res) {
+	  if(res == "farsi") {
+	  	colors = [];
+	  	io.emit('allColors', colors);	//you need to refresh the page to get this to work.. find a way to reset the colors to grey (the colors only keeps track of the changes, so clearing it won't tell the browser to change them back
+	  }
+  });
+  
 });
 http.listen(3030, function(){
   console.log('listening on *:3030');
